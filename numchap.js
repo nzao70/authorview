2fa8c4393aad38d136f81a707716bec6ae28781c
nzao70 <140081328+nzao70@users.noreply.github.com>
2024-02-11 13:52:21 +0100

    Update numchap.js

--- numchap.js
+++ numchap.js
@@ -1,20 +1,22 @@
- function reloadScript() {
+function reloadScript() {
                                     const currentPage = document.querySelector("#pageTitle").innerText;
                                     const listContainer = document.getElementById('listContainerchap');
 
                                     listContainer.innerHTML = '';
 
-                                    for (let i = 1; i <= 3; i++) {
-                                        const linkText = 'Tome-' + i;// tome ou chapitre 
+                                    for (let i = 1; i <= 5; i++) {
+                                        const linkText = 'Chapitre-' + i;// Chapitre ou chapitre 
                                         const isActive = currentPage === linkText;
 
                                         if (isActive) {
                                             listContainer.innerHTML += '<li style="color: red;">' + linkText + '</li>';
                                         } else {
-                                            // activateFunction(\'Tome' + i + '\') active la fonction Tome1_enable() modifie bien tome en chapitre ou volume ou tome
-                                            listContainer.innerHTML += '<li class="serie-button" id="bt' + i + '" onclick="activateFunction(\'Tome' + i + '\')"><a href="#">' + linkText + '</a></li>';
+                                            // activateFunction(\'Chapitre' + i + '\') active la fonction Chapitre1_enable() modifie bien Chapitre en chapitre ou volume ou Chapitre
+                                            listContainer.innerHTML += '<li class="serie-button" id="bt' + i + '" onclick="activateFunction(\'Chapitre' + i + '\')"><a href="#">' + linkText + '</a></li>';
                                         }
                                     }
                                 }
+
+                                document.addEventListener('DOMContentLoaded', reloadScript);
 
                                 document.addEventListener('DOMContentLoaded', reloadScript);
