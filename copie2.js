778f91fd9c9b3d57da95d0b55697be73c6d7582d
nzao70 <140081328+nzao70@users.noreply.github.com>
2024-12-20 12:59:09 +0100

    Update and rename copie1.js to copie2.js

--- copie2.js
+++ copie2.js
@@ -1,9 +1,6 @@
 function copyText(text) {
             // Copier le texte dans le presse-papier
             navigator.clipboard.writeText(text)
-                .then(() => {
-                    alert(`Texte copié : "${text}"`);
-                })
                 .catch(err => {
                     console.error('Erreur lors de la copie :', err);
                 });
